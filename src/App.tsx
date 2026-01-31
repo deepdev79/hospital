acfc197fe1eb5e18fc185df40e542164817a73ac
Deepankar <deepankar7493@gmail.com>
2026-01-31 15:29:41 +0530

    setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold text-sky-400">
+        Vite + Tailwind + Router + Redux
+      </h1>
+      <p className="mt-4 text-slate-400">Setup Successful!</p>
+    </div>
+  );
+}
+
+export default App;
